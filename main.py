547c294ccb20989d62d1951438c81220efd3ea10
Zara Zorin <BillsgWu@outlook.com>
2025-12-08 23:09:22 +0800

    1.1-251208-beta CPU perform++

--- main.py
+++ main.py
@@ -26,50 +26,52 @@ def draw():
     speed = round(speed * SPEED_DACAY,5)
     mouse = pygame.mouse.get_pos()
     select,ind = get_selected(mouse)
-    for galaxyname in imgls:
-        for j in sidls[galaxyname]:
-            j[0] += speed
-        galaxycenter[galaxyname][0] += speed
-        dist = (mouse[0] - galaxycenter[galaxyname][0])**2 + (mouse[1] - galaxycenter[galaxyname][1])**2
-        linecolor = [(i[0] - i[1]) * (LINE_SHOW_FACTOR - alpha) / LINE_SHOW_FACTOR * max(VISIBLE_DISTANCE**2-dist,0)/VISIBLE_DISTANCE**2 + i[1] for i in zip(LINE_COLOR,BG_COLOR)]
-        fontcolor = [(i[0] - i[1]) * (LABEL_SHOW_FACTOR - alpha) / LABEL_SHOW_FACTOR * max(VISIBLE_DISTANCE**2-dist,0)/VISIBLE_DISTANCE**2 + i[1] for i in zip(LABEL_COLOR,BG_COLOR)]
-        galnamesurf = labelfont.render(galaxyname,1,fontcolor)
-        if not galaxylabel[galaxyname]:
-            screen.blit(galnamesurf,[galaxycenter[galaxyname][0] - (galnamesurf.get_width()/2),galaxycenter[galaxyname][1] - (galnamesurf.get_height()/2)])
-        else:
-            labelsurf = labelfont.render(galaxylabel[galaxyname],1,fontcolor)
-            screen.blit(galnamesurf,[galaxycenter[galaxyname][0] - (galnamesurf.get_width()/2),galaxycenter[galaxyname][1] - (galnamesurf.get_height()/2) - LABEL_DISPSIZE / 2])
-            screen.blit(labelsurf,[galaxycenter[galaxyname][0] - (labelsurf.get_width()/2),galaxycenter[galaxyname][1] - (labelsurf.get_height()/2) + LABEL_DISPSIZE / 2])
-        pygame.draw.aalines(screen,linecolor,False,sidls[galaxyname])
-        in_screen = False
-        for j in range(len(imgls[galaxyname])):
-            img = starrotated[imgls[galaxyname][j]["type"]][angls[galaxyname][j]]
-            if galaxyname == ind[0] and j == ind[1]:
-                img = pygame.transform.scale2x(img)
-            angls[galaxyname][j] = (angls[galaxyname][j] + imgls[galaxyname][j]["rotate"]) % 360
-            rect = img.get_rect()
-            imgls[galaxyname][j]["pos"][0] += speed
-            if imgls[galaxyname][j]["pos"][0] > 0 and imgls[galaxyname][j]["pos"][0] < screensize[0]:
-                in_screen = True
-            rect.center = imgls[galaxyname][j]["pos"]
-            img.set_alpha(255 - alpha)
-            screen.blit(img,rect)
-        if not in_screen:
-            for j in imgls[galaxyname]:
-                if j["pos"][0] > screensize[0]:
-                    j["pos"][0] -= screensize[0] * 2
-                elif j["pos"][0] < 0:
-                    j["pos"][0] += screensize[0] * 2
+    if alpha != 255:# not showing or
+        screen.fill(BG_COLOR)
+        for galaxyname in imgls:
             for j in sidls[galaxyname]:
-                if j[0] > screensize[0]:
-                    j[0] -= screensize[0] * 2
-                elif j[0] < 0:
-                    j[0] += screensize[0] * 2
-            if galaxycenter[galaxyname][0] > screensize[0]:
-                galaxycenter[galaxyname][0] -= screensize[0] * 2
-            elif galaxycenter[galaxyname][0] < 0:
-                galaxycenter[galaxyname][0] += screensize[0] * 2
-    if showing:
+                j[0] += speed
+            galaxycenter[galaxyname][0] += speed
+            dist = (mouse[0] - galaxycenter[galaxyname][0])**2 + (mouse[1] - galaxycenter[galaxyname][1])**2
+            linecolor = [(i[0] - i[1]) * (LINE_SHOW_FACTOR - alpha) / LINE_SHOW_FACTOR * max(VISIBLE_DISTANCE**2-dist,0)/VISIBLE_DISTANCE**2 + i[1] for i in zip(LINE_COLOR,BG_COLOR)]
+            fontcolor = [(i[0] - i[1]) * (LABEL_SHOW_FACTOR - alpha) / LABEL_SHOW_FACTOR * max(VISIBLE_DISTANCE**2-dist,0)/VISIBLE_DISTANCE**2 + i[1] for i in zip(LABEL_COLOR,BG_COLOR)]
+            galnamesurf = labelfont.render(galaxyname,1,fontcolor)
+            if not galaxylabel[galaxyname]:
+                screen.blit(galnamesurf,[galaxycenter[galaxyname][0] - (galnamesurf.get_width()/2),galaxycenter[galaxyname][1] - (galnamesurf.get_height()/2)])
+            else:
+                labelsurf = labelfont.render(galaxylabel[galaxyname],1,fontcolor)
+                screen.blit(galnamesurf,[galaxycenter[galaxyname][0] - (galnamesurf.get_width()/2),galaxycenter[galaxyname][1] - (galnamesurf.get_height()/2) - LABEL_DISPSIZE / 2])
+                screen.blit(labelsurf,[galaxycenter[galaxyname][0] - (labelsurf.get_width()/2),galaxycenter[galaxyname][1] - (labelsurf.get_height()/2) + LABEL_DISPSIZE / 2])
+            pygame.draw.aalines(screen,linecolor,False,sidls[galaxyname])
+            in_screen = False
+            for j in range(len(imgls[galaxyname])):
+                img = starrotated[imgls[galaxyname][j]["type"]][angls[galaxyname][j]]
+                if galaxyname == ind[0] and j == ind[1]:
+                    img = pygame.transform.scale2x(img)
+                angls[galaxyname][j] = (angls[galaxyname][j] + imgls[galaxyname][j]["rotate"]) % 360
+                rect = img.get_rect()
+                imgls[galaxyname][j]["pos"][0] += speed
+                if imgls[galaxyname][j]["pos"][0] > 0 and imgls[galaxyname][j]["pos"][0] < screensize[0]:
+                    in_screen = True
+                rect.center = imgls[galaxyname][j]["pos"]
+                img.set_alpha(255 - alpha)
+                screen.blit(img,rect)
+            if not in_screen:
+                for j in imgls[galaxyname]:
+                    if j["pos"][0] > screensize[0]:
+                        j["pos"][0] -= screensize[0] * 2
+                    elif j["pos"][0] < 0:
+                        j["pos"][0] += screensize[0] * 2
+                for j in sidls[galaxyname]:
+                    if j[0] > screensize[0]:
+                        j[0] -= screensize[0] * 2
+                    elif j[0] < 0:
+                        j[0] += screensize[0] * 2
+                if galaxycenter[galaxyname][0] > screensize[0]:
+                    galaxycenter[galaxyname][0] -= screensize[0] * 2
+                elif galaxycenter[galaxyname][0] < 0:
+                    galaxycenter[galaxyname][0] += screensize[0] * 2
+    if showing and alpha != 255:
         image.set_alpha(alpha)
         screen.blit(image,loc)
     pygame.display.update()
@@ -88,7 +90,6 @@ showing = False
 alpha = 0
 bgm.play(-1)
 while keepgoing:
-    screen.fill(BG_COLOR)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
@@ -114,7 +115,6 @@ while keepgoing:
                 leftbuttondown = rightbuttondown = False
                 alpha = 0
                 while innerkeepgoing:
-                    screen.fill(BG_COLOR)
                     for event in pygame.event.get():
                         if event.type == pygame.KEYDOWN:
                             if event.key == pygame.K_ESCAPE:
@@ -129,7 +129,6 @@ while keepgoing:
                         alpha = 255
                     draw()
                 while alpha > 0:
-                    screen.fill(BG_COLOR)
                     for event in pygame.event.get():
                         if event.type == pygame.KEYDOWN:
                             if event.key == pygame.K_ESCAPE:
@@ -139,7 +138,6 @@ while keepgoing:
                     alpha -= ALPHA_DACAY
                 alpha = 0
                 showing = False
-                screen.fill(BG_COLOR)
     draw()
 pygame.quit()
 for filename in os.listdir("temp"):
